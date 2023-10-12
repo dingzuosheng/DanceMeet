4b12e7ddb4edd93766c917eaa1187c9262b670e6
Ding, Zuosheng <Zuosheng.Ding@adesso.de>
2023-10-12 18:34:21 +0200

    cancel DanceSkill as entity, delete repo of danceSkill

--- src/main/java/com/example/dancemeet/model/DanceSkill.java
+++ src/main/java/com/example/dancemeet/model/DanceSkill.java
@@ -1,7 +1,6 @@
 package com.example.dancemeet.model;
 
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,14 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-@Entity
-@Table(name = "dance_skill")
 public class DanceSkill {
-    @Id
-    @Column(name = "name")
     private String name;
 
-    @Enumerated(EnumType.ORDINAL)
-    @Column(name = "level")
     private Level level;
 }
